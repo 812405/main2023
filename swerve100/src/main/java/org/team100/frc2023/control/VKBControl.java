9f7ca490c7cd79e945f5c9365d4f54d3cb25dee2
Ishan Harbury <98669601+812405@users.noreply.github.com>
2023-09-15 20:32:23 -0700

    some modifications to VKBControl

--- swerve100/src/main/java/org/team100/frc2023/control/VKBControl.java
+++ swerve100/src/main/java/org/team100/frc2023/control/VKBControl.java
@@ -1,19 +1,44 @@
 package org.team100.frc2023.control;
 
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Twist2d;
+import edu.wpi.first.wpilibj.event.EventLoop;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
-public class VKBControl implements Control{
+public class VKBControl implements Control {
 
     private final CommandJoystick m_controller;
     private Rotation2d previousRotation = new Rotation2d(0);
-    
+    private double ticks = 0;
+    private final double kTicksToDegrees = 0;
+    private final double kTicksToRadians = 0;
+
+    public static class Config {
+        public static int kTriggerSoftChannel = 0;
+        public static int kTriggerHardChannel = 1;
+        public static int kBigRedButtonChannel = 2;
+        public static int kHighGreyButtonChannel = 3;
+        public static int kMidGreyButtonChannel = 4;
+        public static int kLowGreyButtonChannel = 5;
+        public static int kF1ButtonChannel = 6;
+        public static int kF2ButtonChannel = 7;
+        public static int kF3ButtonChannel = 8;
+        public static int kSw1UpChannel = 9;
+        public static int kSw1DownChannel = 10;
+        public static int kEn1IncChannel = 11;
+        public static int kEn1DecChannel = 12;
+    }
+
     public VKBControl() {
         m_controller = new CommandJoystick(0);
         System.out.printf("Controller0: %s\n", m_controller.getHID().getName());
+
+        EventLoop loop = CommandScheduler.getInstance().getActiveButtonLoop();
+        loop.bind(updateTicks());
     }
 
     @Override
@@ -34,19 +59,52 @@ public class VKBControl implements Control{
         return previousRotation;
     }
 
+    public double getEn1Raw() {
+        return ticks;
+    }
+
+    public double getEn1Radians() {
+        return MathUtil.angleModulus(ticks * kTicksToRadians);
+    }
+
+    public double getEn1Degrees() {
+        return ticks * kTicksToDegrees - 360 * Math.floor(ticks * kTicksToDegrees / 360);
+    }
+
     @Override
     public void resetRotation0(Command command) {
-        button(0).onTrue(command);
+        button(Config.kSw1UpChannel).onTrue(command);
     }
 
     @Override
     public void resetRotation180(Command command) {
-        button(0).onTrue(command);
+        button(Config.kSw1DownChannel).onTrue(command);
     }
 
     @Override
     public void defense(Command command) {
-        button(0).whileTrue(command);
+        button(Config.kBigRedButtonChannel).whileTrue(command);
+    }
+
+    private Runnable updateTicks() {
+        return new Runnable() {
+            private boolean m_incLast = button(Config.kEn1IncChannel).getAsBoolean();
+            private boolean m_decLast = button(Config.kEn1DecChannel).getAsBoolean();
+
+            @Override
+            public void run() {
+                boolean inc = button(Config.kEn1IncChannel).getAsBoolean();
+                boolean dec = button(Config.kEn1DecChannel).getAsBoolean();
+                if (!m_incLast && inc) {
+                    ticks++;
+                }
+                if (!m_decLast && dec) {
+                    ticks--;
+                }
+                m_incLast = inc;
+                m_decLast = dec;
+            }
+        };
     }
 
     private JoystickButton button(int button) {
